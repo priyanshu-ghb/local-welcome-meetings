096ce331bc4ecd164fa430da2cf54ae9010d7ce6
Jan Baykara <janbaykara@pm.me>
2021-08-27 17:54:03 +0100

    Fix import path

--- pages/api/updateUserProfile.ts
+++ pages/api/updateUserProfile.ts
@@ -1,6 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import { getUserFromHTTPRequest, isValidLeaderEmail, upsertUserProfile } from '../../data/leader'
+import { isValidLeaderEmail, upsertUserProfile } from '../../data/leader'
 import { strict as assert } from 'assert';
+import { getUserFromHTTPRequest } from '../../data/leader-shared';
 
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
   const { user } = await getUserFromHTTPRequest(req)
