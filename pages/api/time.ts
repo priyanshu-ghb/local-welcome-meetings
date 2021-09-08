e3de4ecd3cdc30b010f4e3bf66c06a0d82706883
Jan Baykara <janbaykara@pm.me>
2021-09-08 17:05:00 +0100

    Try and be more precise about the returned time

--- pages/api/time.ts
+++ pages/api/time.ts
@@ -4,5 +4,5 @@ import { corsGET, runMiddleware } from '../../utils/cors';
 export default async function handler (req: NextApiRequest, res: NextApiResponse<any>) {
   await runMiddleware(req, res, corsGET)
   const time = new Date()
-  return res.status(200).json({ time })
+  return res.status(200).json({ time: time.getTime() })
 }
