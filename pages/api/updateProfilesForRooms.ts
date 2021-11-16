9b246bed24e236e24da87353313e08dc2e69347c
Jan Baykara <janbaykara@pm.me>
2021-11-16 18:34:52 +0000

    Check all rooms for permissions, using default Hubspot list if required

--- pages/api/updateProfilesForRooms.ts
+++ pages/api/updateProfilesForRooms.ts
@@ -14,62 +14,59 @@ async function handler (req: NextApiRequest, res: NextApiResponse) {
   const oldPermissions = await supabase.from<RoomPermission>('roompermission').select('*')
   assert(Array.isArray(oldPermissions.data), 'Couldnt fetch permissions')
 
-  const defaultHubspotLeaderListId = env.get('HUBSPOT_LEADER_LIST_ID').required().asInt()
-
   for (const room of rooms.data) {
-    if (room.hubspotLeaderListId) {
-      /** Upsert room leader's profiles from Hubspot */
+    /** Upsert room leader's profiles from Hubspot */
 
-      const { contacts } = await getHubspotContactsInList(room.hubspotLeaderListId || defaultHubspotLeaderListId)
+    const hubspotLeaderListId = room.hubspotLeaderListId || env.get('HUBSPOT_LEADER_LIST_ID').required().asInt()
+    const { contacts } = await getHubspotContactsInList(hubspotLeaderListId)
 
-      let roomLeaderProfiles = await upsertUserProfile(contacts.map(contact => ({
-        email: getDetailsForContact(contact).EMAIL?.[0].value,
-        hubspotContactId: contact.vid as any,
-        firstName: contact.properties.firstname.value,
-        lastName: contact.properties.lastname.value,
-        canLeadSessions: true
-      })))
+    let roomLeaderProfiles = await upsertUserProfile(contacts.map(contact => ({
+      email: getDetailsForContact(contact).EMAIL?.[0].value,
+      hubspotContactId: contact.vid as any,
+      firstName: contact.properties.firstname.value,
+      lastName: contact.properties.lastname.value,
+      canLeadSessions: true
+    })))
 
-      assert(roomLeaderProfiles.body?.length, 'Couldnt upsert roomLeaderProfiles')
+    assert(roomLeaderProfiles.body?.length, 'Couldnt upsert roomLeaderProfiles')
 
-      /** Rebuild room permissions */
+    /** Rebuild room permissions */
 
-      // Add permissions for new roomLeaderProfiles
+    // Add permissions for new roomLeaderProfiles
 
-      const currentPermissions = await supabase.from<RoomPermission>('roompermission').select('*')
+    const currentPermissions = await supabase.from<RoomPermission>('roompermission').select('*')
+    .match({
+      roomId: room.id,
+      type: RoomPermissionType.Lead
+    })
+
+    const futurePermissions = roomLeaderProfiles.body.map(profile => ({
+      roomId: room.id,
+      profileId: profile.id,
+      type: RoomPermissionType.Lead
+    }))
+
+    // New leaders to the group will have permissions added
+    const newNext = futurePermissions.filter(p => !currentPermissions.body?.some(cP => cP.profileId === p.profileId))
+
+    const newPermissions = await supabase.from<RoomPermission>('roompermission')
+      .insert(
+        futurePermissions.filter(p =>
+          !currentPermissions.body?.some(cP => cP.profileId === p.profileId))
+      )
+
+    // Leaders no longer in this group will have the permission deleted
+    const deletedPermissions = await supabase.from<RoomPermission>('roompermission').delete()
       .match({
         roomId: room.id,
         type: RoomPermissionType.Lead
       })
-
-      const futurePermissions = roomLeaderProfiles.body.map(profile => ({
-        roomId: room.id,
-        profileId: profile.id,
-        type: RoomPermissionType.Lead
-      }))
-
-      // New leaders to the group will have permissions added
-      const newNext = futurePermissions.filter(p => !currentPermissions.body?.some(cP => cP.profileId === p.profileId))
-
-      const newPermissions = await supabase.from<RoomPermission>('roompermission')
-        .insert(
-          futurePermissions.filter(p =>
-            !currentPermissions.body?.some(cP => cP.profileId === p.profileId))
-        )
-
-      // Leaders no longer in this group will have the permission deleted
-      const deletedPermissions = await supabase.from<RoomPermission>('roompermission').delete()
-        .match({
-          roomId: room.id,
-          type: RoomPermissionType.Lead
-        })
-        .match({
-          profileId: currentPermissions.body
-            ?.filter(cp =>
-              !futurePermissions.some(fp => fp.profileId === cp.profileId))
-                .map(cp => cp.profileId)
-        })
-    }
+      .match({
+        profileId: currentPermissions.body
+          ?.filter(cp =>
+            !futurePermissions.some(fp => fp.profileId === cp.profileId))
+              .map(cp => cp.profileId)
+      })
   }
   
   // console.debug("Room permissions and profiles updated OK")
